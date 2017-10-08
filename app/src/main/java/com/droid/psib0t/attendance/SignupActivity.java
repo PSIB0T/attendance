d1ac915e1d4a0242091ce73fc99bbb931506ea92
Arvind Narayanan <arvindonarayanan@gmail.com>
2017-10-08 07:54:30 +0530

    Add: firebase support

--- app/src/main/java/com/droid/psib0t/attendance/SignupActivity.java
+++ app/src/main/java/com/droid/psib0t/attendance/SignupActivity.java
@@ -3,8 +3,19 @@ package com.droid.psib0t.attendance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
