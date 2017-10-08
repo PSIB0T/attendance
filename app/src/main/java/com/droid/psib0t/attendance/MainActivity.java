db9bc3c0414ca35a65555655f8eb8956238b1f62
Arvind Narayanan <arvindonarayanan@gmail.com>
2017-10-08 17:47:10 +0530

    Add: Footer nav

--- app/src/main/java/com/droid/psib0t/attendance/MainActivity.java
+++ app/src/main/java/com/droid/psib0t/attendance/MainActivity.java
@@ -2,9 +2,12 @@ package com.droid.psib0t.attendance;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -19,22 +22,42 @@ public class MainActivity extends AppCompatActivity {
     static final int DIALOG_ID = 0;
 
     Calendar calendar;
-    TextView dateText;
+    TextView selectedItemText;
 
     Button datePickerButton;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_favorites:
+                    selectedItemText.setText(R.string.title_home);
+                    return true;
+                case R.id.action_schedules:
+                    selectedItemText.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.action_music:
+                    selectedItemText.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH);
         day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        dateText = (TextView) findViewById(R.id.dateText);
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         datePickerButton = (Button) findViewById(R.id.datePickerButton);
+
+        datePickerButton.setText(day + "/" + month + "/" + year);
+        selectedItemText = (TextView) findViewById(R.id.selectedItem);
 
         datePickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,12 +69,15 @@ public class MainActivity extends AppCompatActivity {
                         month = i1;
                         day = i2;
 
-                        dateText.setText("ggwp  ");
+                        datePickerButton.setText(day + "/" + month + "/" + year);
                     }
                 }, year, month, day);
                 datePickerDialog.show();
             }
         });
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNav);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 
