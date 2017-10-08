b70f74e7ad928a0b92a3c8d446a5413c13c49271
Arvind Narayanan <arvindonarayanan@gmail.com>
2017-10-08 16:46:03 +0530

    Basic datepicker

--- app/src/main/java/com/droid/psib0t/attendance/MainActivity.java
+++ app/src/main/java/com/droid/psib0t/attendance/MainActivity.java
@@ -1,13 +1,58 @@
 package com.droid.psib0t.attendance;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    int year, month, day;
+    static final int DIALOG_ID = 0;
+
+    Calendar calendar;
+    TextView dateText;
+
+    Button datePickerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dateText = (TextView) findViewById(R.id.dateText);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePickerButton = (Button) findViewById(R.id.datePickerButton);
+
+        datePickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        year = i;
+                        month = i1;
+                        day = i2;
+
+                        dateText.setText("ggwp  ");
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
+
+
 }
